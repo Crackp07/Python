ccf434824cc9c2713e3c95118a5eef2e0419b6fd
Crackp07 <byushuk@gmail.com>
2023-09-22 20:55:38 +0300

    Python

--- hw/1.py
+++ hw/1.py
@@ -1,7 +1,13 @@
-num1 = int(input('Введіть перше чисто: '))
-num2 = int(input('Введіть друге число: '))
-num3 = int(input('Введіть трете число: '))
-sum_result = num1 + num2 + num3
-product_result = num1 * num2 * num3
-print('Сумма чисел: ', sum_result)
-print('Произведение чисел: ', product_result)
+# num1 = int(input('Введіть перше чисто: '))
+# num2 = int(input('Введіть друге число: '))
+# num3 = int(input('Введіть трете число: '))
+# sum_result = num1 + num2 + num3
+# product_result = num1 * num2 * num3
+# print('Сумма чисел: ', sum_result)
+# print('Произведение чисел: ', product_result)
+
+zarplata = int(input("Зарплата за місяць: "))
+kredit = int(input("Сума місячного платежу по кредиту: "))
+komunalni_poslugi = int(input("Задолженісь за комунальні послуги: "))
+zalishok = zarplata - kredit - komunalni_poslugi
+print("Залишок після виплат: ", zalishok)

904ddcd483090b5ec3717d87de789a79451d27db
Crackp07 <byushuk@gmail.com>
2023-09-22 20:39:57 +0300

    Python

---
+++ hw/1.py
@@ -0,0 +1,7 @@
+num1 = int(input('Введіть перше чисто: '))
+num2 = int(input('Введіть друге число: '))
+num3 = int(input('Введіть трете число: '))
+sum_result = num1 + num2 + num3
+product_result = num1 * num2 * num3
+print('Сумма чисел: ', sum_result)
+print('Произведение чисел: ', product_result)
---
+++ hw/2.py
@@ -0,0 +1,8 @@
+# a = input('Input a: ')
+# b = input('Input b: ')
+# c = input('Input c: ')
+# result = a + b + c
+# final_number = int(result)
+# print ('Number: ', final_number)
+
+number = input('Введіть число (4 цифри): ')

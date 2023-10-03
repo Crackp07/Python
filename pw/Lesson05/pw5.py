81d199891a0754f510583dcbeb03e6720e13340d
Crackp07 <byushuk@gmail.com>
2023-10-03 20:51:50 +0300

    Python

--- pw/Lesson05/pw5.py
+++ pw/Lesson05/pw5.py
@@ -3,13 +3,17 @@
 # reversed_string = input_string[::-1]
 # print("Обернутий рядок:", reversed_string)
 # 2
+# input_string = input("Введіть рядок: ")
+# l = 0
+# d = 0
+# for i in input_string:
+#     if i.isalpha(): 
+#         l += 1
+#     elif i.isdigit():
+#         d += 1
+# print("Кількість букв у рядку:", l)
+# print("Кількість цифр у рядку:", d)
+# 3
 input_string = input("Введіть рядок: ")
-l = 0
-d = 0
-for i in input_string:
-    if i.isalpha(): 
-        l += 1
-    elif i.isdigit():
-        d += 1
-print("Кількість букв у рядку:", l)
-print("Кількість цифр у рядку:", d)
+search = input("Введіть символ для пошуку: ")
+count = 0 

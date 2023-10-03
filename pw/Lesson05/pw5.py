ef9f56f9dd59083eaf657bd67821c4f03716ab0a
Crackp07 <byushuk@gmail.com>
2023-10-03 20:39:10 +0300

    Python

--- pw/Lesson05/pw5.py
+++ pw/Lesson05/pw5.py
@@ -4,12 +4,12 @@
 # print("Обернутий рядок:", reversed_string)
 # 2
 input_string = input("Введіть рядок: ")
-letter_count = 0
-digit_count = 0
-for char in input_string:
-    if char.isalpha():
-        letter_count += 1
-    elif char.isdigit():
-        digit_count += 1
-print("Кількість букв у рядку:", letter_count)
-print("Кількість чисел у рядку:", digit_count) 
+l = 0
+d = 0
+for i in input_string:
+    if i.isalpha(): 
+        l += 1
+    elif i.isdigit():
+        d += 1
+print("Кількість букв у рядку:", l)
+print("Кількість цифр у рядку:", d)

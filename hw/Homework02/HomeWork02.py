bd51aebc24adfb051b9722bd25de661fddb9033a
Crackp07 <byushuk@gmail.com>
2023-10-11 12:29:33 +0300

    Python

--- hw/Homework02/HomeWork02.py
+++ hw/Homework02/HomeWork02.py
@@ -1,3 +1,4 @@
+# 1
 # num1 = int(input('Введіть перше чисто: '))
 # num2 = int(input('Введіть друге число: '))
 # num3 = int(input('Введіть трете число: '))
@@ -5,9 +6,56 @@
 # product_result = num1 * num2 * num3
 # print('Сумма чисел: ', sum_result)
 # print('Произведение чисел: ', product_result)
-
+# 2
 # zarplata = int(input("Зарплата за місяць: "))
 # kredit = int(input("Сума місячного платежу по кредиту: "))
 # komunalni_poslugi = int(input("Задолженісь за комунальні послуги: "))
 # zalishok = zarplata - kredit - komunalni_poslugi
 # print("Залишок після виплат: ", zalishok)
+# 3
+# D1 = int(input("Введіть довжину першої діагоналі ромба: "))
+# D2 = int(input("Введіть довжину другої діагоналі ромба: "))
+# area = (D1 * D2)/2
+# print(f"Площа ромба з діагоналями {D1} і {D2} дорівню {area}")
+# 4
+# print("To be\nor not\nto be")
+# 5
+# quote = "Life is what happens\nwhen\nyou're busy making other plans\nJohn Lennon."
+# print(quote)
+# 6
+# a = int(input("Введіть перше число: "))
+# b = int(input("Введіть друге число: "))
+# c = int(input("Введіть третє число: "))
+# result = a * 100 + b * 10 + c 
+# print("Створене чило: ", result)
+# 7
+# number = int(input("Введіть число з чотирьох цифр: "))
+# a = number // 1000
+# b = (number % 1000) // 100
+# c = (number % 100) // 10
+# d = number % 10
+# product = a * b * c * d
+# print(f"Результат добутку цифр: {a}*{b}*{c}*{d}={product}")
+# 8
+# met = int(input("Введіть кількість метрі: "))
+# centimet = met * 100
+# decimet = met * 10
+# millimet = met * 1000
+# miles = met * 0.000621371
+# print(f"{met} метрів дорівнює {centimet} сантиметрів")
+# print(f"{met} метрів дорівнює {decimet} дециметрів")
+# print(f"{met} метрів дорівнює {millimet} міліметрів")
+# print(f"{met} метрів дорівнює {miles} миль")
+# 9
+# l = int(input("Введіть довжину основи трикутника: "))
+# h = int(input("Введіть довжину висоти трикутника: "))
+# s = (l*h)/2
+# print(f"Площа трикутнака дорівнює", s) 
+# 10
+n = int(input("Введіть чотирьохзначне число: "))
+a = n % 10
+b = (n // 10) % 10
+c = (n // 100) % 10
+d = (n // 1000) % 10
+reversed_number = a * 1000 + b * 100 + c * 10 + d
+print(f"Результат перевертання числа: ", reversed_number)
--- pw/Lesson07/pw7.py
+++ pw/Lesson07/pw7.py
@@ -1,48 +1,60 @@
 # 1
-class Human:
-    def __init__(self):
-        self.fullname = ''
-        self.phone = ''
-        self.birthday = ''
-        self.city = ''
-        self.country = ''
-        self.homeadress = ''
+# class Human:
+#     def __init__(self):
+#         self.fullname = ''
+#         self.phone = ''
+#         self.birthday = ''
+#         self.city = ''
+#         self.country = ''
+#         self.homeadress = ''
 
-    def getFullname(self):
-        return self.fullname
-    def getPhone(self):
-        return self.phone
-    def getBirthday(self):
-        return self.birthday
-    def getCity(self):
-        return self.city
-    def getCountry(self):
-        return self.country
-    def getHomeadress(self):
-        return self.homeadress
+#     def getFullname(self):
+#         return self.fullname
+#     def getPhone(self):
+#         return self.phone
+#     def getBirthday(self):
+#         return self.birthday
+#     def getCity(self):
+#         return self.city
+#     def getCountry(self):
+#         return self.country
+#     def getHomeadress(self):
+#         return self.homeadress
     
-    def inputdata(self):
-        self.fullname = input("Введіть повне ім'я: ")
-        self.phone = input("Ведіть номер телефона: ")
-        self.birthday = input("Введіть дату народження: ")
-        self.city = input("Введіть місто: ")
-        self.country = input("Введіть країну: ")
-        self.homeadress = input("Введіть домашню адресу: ")
+#     def inputdata(self):
+#         self.fullname = input("Введіть повне ім'я: ")
+#         self.phone = input("Ведіть номер телефона: ")
+#         self.birthday = input("Введіть дату народження: ")
+#         self.city = input("Введіть місто: ")
+#         self.country = input("Введіть країну: ")
+#         self.homeadress = input("Введіть домашню адресу: ")
 
-    def showdata(self):
-        print("Повне ім'я: ", self.fullname)
-        print("Номер телефону: ", self.phone)
-        print("Дата народження: ", self.birthday)
-        print("Країна: ", self.country)
-        print("Місто: ", self.city)
-        print("Домашня адреса: ", self.homeadress)
+#     def showdata(self):
+#         print("Повне ім'я: ", self.fullname)
+#         print("Номер телефону: ", self.phone)
+#         print("Дата народження: ", self.birthday)
+#         print("Країна: ", self.country)
+#         print("Місто: ", self.city)
+#         print("Домашня адреса: ", self.homeadress)
 
-h1 = Human()
-h1.inputdata()
-print("\nВведені дані: ") 
-h1.showdata()
-# для отримання окремих полів
-print("\nПовне ім'я:",h1.getFullname())
-
+# h1 = Human()
+# h1.inputdata()
+# print("\nВведені дані: ") 
+# h1.showdata()
+# # для отримання окремих полів
+# print("\nПовне ім'я:",h1.getFullname())
+# print("Дата народження:", h1.getBirthday())
+# print("Номер телефону: ", h1.getPhone())
+# print("Країна: ", h1.getCountry())
+# print("Місто: ", h1.getCity())
+# print("Домашня адреса: ", h1.getHomeadress())
 # 2
 # class City:
+#     def __init__(self):
+#         self.city = ''
+#         self.region = ''
+#         self.country = ''
+#         self.population = 0
+#         self.postcode = ''
+#         self.phonecode = ''
+#     def inputdata(self):
